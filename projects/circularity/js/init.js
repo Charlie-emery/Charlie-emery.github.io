c379ce9d3f9b49ae4dd0e949cbcd80787145b7ec
Charlie-emery <AA.charlie.emery@gmail.com>
2024-12-05 01:32:56 +0000

    add bouncing box

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -31,7 +31,7 @@ var init = function (window) {
             circles.push(circle);}
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        for (var i = 0; i <= 100; i++){
+        for (var i = 1; i <= 100; i++){
             drawCircle()
         }
     
