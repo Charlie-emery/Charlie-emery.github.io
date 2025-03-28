6f7574832cedaf269044970aecfd38c10ddcd41b
Charlie-emery <AA.charlie.emery@gmail.com>
2025-03-28 01:00:35 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -1,8 +1,8 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -10,14 +10,24 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+  };
+  var walker = {
+    positionX: 0,
+    positionY: 0,
+    speedX: 0,
+    speedY: 0,
+  }
   // Game Item Objects
 
-
   // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL); // execute newFrame every 0.0166 seconds (60 Frames per second)
+  $(document).on("keydown", handleKeyDown); // change 'eventType' to the type of event you want to handle
+  $(document).on("keyup", handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -27,22 +37,49 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
 
+
+    repositionGameItem();
+    redrawGameItem();
   }
-  
+
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    console.log(event.which);
+    if (event.which === KEY.UP) {
+      walker.speedY = -5;
+    }
+     if (event.which === KEY.LEFT) {
+      walker.speedX = -5;
+    }
+    if (event.which === KEY.DOWN) {
+      walker.speedY = 5;
+    }
+    if (event.which === KEY.RIGHT) {
+      walker.speedX = 5;
+    }
+    
   }
-
+function handleKeyUp(event){
+  if (event.which === KEY.UP) {
+    walker.speedY = 0;
+  }
+   if (event.which === KEY.LEFT) {
+    walker.speedX = 0;
+  }
+  if (event.which === KEY.DOWN) {
+    walker.speedY = 0;
+  }
+  if (event.which === KEY.RIGHT) {
+    walker.speedX = 0;
+  }
+}
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -50,5 +87,13 @@ function runProgram(){
     // turn off event handlers
     $(document).off();
   }
-  
+  function repositionGameItem(){
+    walker.positionX += walker.speedX;
+    walker.positionY += walker.speedY;
+  }
+  function redrawGameItem(){
+    $("#walker").css("left", walker.positionX)
+    $("#walker").css("top", walker.positionY)
+  }
+  function wallCollision
 }
