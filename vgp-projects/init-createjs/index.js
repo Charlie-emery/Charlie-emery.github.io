46fcfd9aea16fbcf21bd4eec556d9488bb3eb600
Charlie-emery <AA.charlie.emery@gmail.com>
2025-09-24 00:49:37 +0000

    face

--- vgp-projects/init-createjs/index.js
+++ vgp-projects/init-createjs/index.js
@@ -6,7 +6,8 @@
   // TODO 5: Initialize CreateJS //
   const canvas = document.getElementById("canvas")
   const stage = new createjs.Stage(canvas);
-
+  var speedX = 1;
+  var speedY = -1;
   // TODO 6: Set the framerate of the Ticker
   createjs.Ticker.framerate = 60
 
@@ -23,31 +24,72 @@
     
   // CREATE A BACKGROUND //
 const bg = new createjs.Shape()
-    bg.graphics.beginFill("#F00").drawCircle(250, 250, 270)
+    bg.graphics.beginFill("rgba(255, 255, 255, 1)").drawCircle(300, 300, 270)
   // CREATE A CIRCLE //
+  const shirt = new createjs.Shape()
+shirt.graphics.beginFill("rgba(187, 0, 255, 1)").drawCircle(300, 700, 300 )
+
+  const hair = new createjs.Shape();
+hair.graphics.beginFill("rgba(90, 173, 12, 1)").drawCircle(60, 50, 50)
+hair.scaleX = 5
+hair.scaleY = 2
+
 const eyeContainer = new createjs.Container();
 eyeContainer.x = 150
-eyeContainer.y= 150
+eyeContainer.y= 200
+const leftP = new createjs.Shape();
+const rightP = new createjs.Shape();
 const leftEye = new createjs.Shape();
 const rightEye = new createjs.Shape();
+const leftM = new createjs.Shape();
+const rightM = new createjs.Shape();
 
-leftEye.graphics.beginFill("rgba(0, 68, 255, 1)").drawRect(25, 25, 60, 10)
-rightEye.graphics.beginFill("rgba(255, 213, 0, 1)").drawCircle(200, 25, 25)
+leftP.graphics.beginFill("rgba(0, 0, 0, 1)").drawCircle(25, 25, 15)
+rightP.graphics.beginFill("rgba(0, 0, 0, 1)").drawCircle(250, 25, 15)
+leftEye.graphics.beginFill("rgba(255, 255, 255, 1)").drawCircle(25, 50, 35)
+leftEye.scaleY= 0.5
+rightEye.graphics.beginFill("rgba(255, 255, 255, 1)").drawCircle(250, 50, 35)
+rightEye.scaleY= 0.5
+leftM.graphics.beginFill("rgba(0, 0, 0, 1)").drawCircle(25, 25, 60)
+rightM.graphics.beginFill("rgba(0, 0, 0, 1)").drawCircle(250, 25, 60)
+
+const ls = new createjs.Container();
+const lipstick = new createjs.Shape();
+const wing1 = new createjs.Shape();
+const wing2 = new createjs.Shape();
+
+lipstick.graphics.beginFill("rgba(255, 0, 0, 1)").drawCircle(75, 410, 50)
+lipstick.scaleX = 4
+lipstick.scaleY = 1.1
+wing1.graphics.beginFill("rgba(255, 0, 0, 1)").drawCircle(320, 830, 50)
+wing1.scaleX = 2
+wing1.skewY = -40
+wing2.graphics.beginFill("rgba(255, 0, 0, 1)").drawCircle(70, 330, 50)
+wing2.scaleX = 2
+wing2.skewY = 40
 
 const mc = new createjs.Container()
 const mouth = new createjs.Shape();
+const top = new createjs.Shape();
 const lip = new createjs.Shape();
-mouth.graphics.beginFill("rgba(0, 0, 0, 1)").drawCircle(80, 400, 50)
+
+mouth.graphics.beginFill("rgba(0, 0, 0, 1)").drawCircle(100, 450, 50)
 mouth.scaleX = 3
-mouth.graphics.beginFill("rgba(255, 0, 0, 1)").drawCircle(80, 370, 50)
-mouth.scaleX = 3
+lip.graphics.beginFill("rgba(255, 255, 255, 1)").drawCircle(100, 411, 50)
+lip.scaleX = 3
+top.graphics.beginFill("rgba(255, 0, 0, 1)").drawCircle(100, 420, 50)
+top.scaleX = 3
   
 
 
+
+
   // ADD DISPLAY OBJECTS TO STAGE //
-  mc.addChild(mouth, lip)
-  eyeContainer.addChild(leftEye, rightEye)
-stage.addChild(bg, eyeContainer, mc)
+  ls.addChild(lipstick, wing1, wing2)
+  mc.addChild(mouth, top, lip)
+  eyeContainer.addChild(leftM, rightM, leftEye, rightEye, leftP, rightP )
+  
+stage.addChild(shirt, hair, bg, eyeContainer, ls, mc)
 
 
 stage.update()
@@ -66,15 +108,27 @@ stage.update()
    * changes to assets, it must call stage.update(); 
    */
   function update(event){
+    leftM.x += speedX
+    leftEye.x += speedX
+    leftP.x += speedX
+     rightM.x += speedY
+    rightEye.x += speedY
+    rightP.x += speedY
     
+    if (leftEye.x >= 220){
+     speedX = -speedX
+   }else if(leftEye <= 55){
+  
+  speedX = -speedX  
+  }
+    if (rightEye.x <= -220){
+     speedY = -speedY
+   }else if(rightEye >= -55){
+  
+  speedY = -speedY
+  }
+   
     
-   if (leftEye.x <=200){
-    leftEye.x++
-   }else{
-    while(leftEye.x >= 25){
-      leftEye.x = leftEye - 0.1
-    }
-   }
     stage.update();
   }
   
