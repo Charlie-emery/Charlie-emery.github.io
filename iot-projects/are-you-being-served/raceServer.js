a14d01365dc00581822f540fe6d55f28f64184b6
Charlie-emery <AA.charlie.emery@gmail.com>
2026-02-24 19:59:22 -0600

    12

--- iot-projects/are-you-being-served/raceServer.js
+++ iot-projects/are-you-being-served/raceServer.js
@@ -3,73 +3,71 @@ const async = require("async");
 
 const port = 8686;
 
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
 
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    
     let racers = ["Green Ambler", "Catalack", "Steel Runner", "G.I. Jogger"];
 
     // TODO 6: Get the start time for the race
     let d = new Date();
-let startTime = d.getTime();
-
+    let startTime = d.getTime();
 
     // TODO 10: Make the whole thing parallel
-    async.parallel( 
-        // TODO 8: Supply an array of functions
-        [function  (callback) {
-    wrapper(callback);
-  },
-  function  (callback) {
-    wrapper(callback);
-  },
-  function  (callback) {
-    wrapper(callback);
-  },
-  function  (callback) {
-    wrapper(callback);
-  },
-],
-        function (error, results) {
-            // TODO 9: add a callback function to the end of the async call to tally the results 
-res.write("Results:\n");
-var victoryOrder = sortTogether(racers, results);
-for(i = 0;i >= victoryOrder.length; i++){
-    res.write(racerName + "\n");
-}
-let d = new Date();
-let endTime = d.getTime();
-let duration = endTime - startTime;
-res.end("Race Duration: " + duration + "ms\n");
-
+    async.parallel(
+      // TODO 8: Supply an array of functions
+      [
+        function (callback) {
+          wrapper(callback);
+        },
+        function (callback) {
+          wrapper(callback);
+        },
+        function (callback) {
+          wrapper(callback);
+        },
+        function (callback) {
+          wrapper(callback);
+        },
+      ],
+      function (error, results) {
+        // TODO 9: add a callback function to the end of the async call to tally the results
+        res.write("Results:\n");
+        var victoryOrder = sortTogether(racers, results);
+        for (i = 0; i >= victoryOrder.length; i++) {
+          res.write(racerName + "\n");
         }
+        let d = new Date();
+        let endTime = d.getTime();
+        let duration = endTime - startTime;
+        res.end("Race Duration: " + duration + "ms\n");
+      },
     );
-    
-}).listen(port);
+  })
+  .listen(port);
 
 // TODO 7: create a common function to be called by all functions in the array passed to the async function
-function wrapper(callback){
-setTimeout(T,Math.random() * 1000)
+function wrapper(callback) {
+  setTimeout(T, Math.random() * 1000);
 }
-function T {
-    var d = new Date()
-    callback(null, d.getTime)
+function T() {
+  var d = new Date();
+  callback(null, d.getTime);
 }
 // sortTogether takes in an array of racer names and an array of times that the racers finished the race.
 // It returns a new array of names, with the list or racers sorted by the time that they finished.
 function sortTogether(names, times) {
-    var tempList = [];
-    for (var i = 0; i < names.length; i++) {
-        tempList.push({'name': names[i], 'time': times[i]});
-    }
+  var tempList = [];
+  for (var i = 0; i < names.length; i++) {
+    tempList.push({ name: names[i], time: times[i] });
+  }
 
-    tempList.sort(function(a, b) {
-        return ((a.time < b.time) ? -1 : ((a.time == b.time) ? 0 : 1));
-    });
+  tempList.sort(function (a, b) {
+    return a.time < b.time ? -1 : a.time == b.time ? 0 : 1;
+  });
 
-    for (var i = 0; i < tempList.length; i++) {
-        names[i] = tempList[i].name;
-    }
-    return names;
+  for (var i = 0; i < tempList.length; i++) {
+    names[i] = tempList[i].name;
+  }
+  return names;
 }
